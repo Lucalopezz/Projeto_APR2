c30bb9e089f61c93b6a7654960f3cc7902e5b95d
Lucas <lucaslops12@gmail.com>
2025-09-29 17:30:41 -0300

    prototipação inicial das funções principais

--- main.c
+++ main.c
@@ -9,6 +9,10 @@
 #define MAX_ENDERECO 200
 #define MAX_REGISTROS 1000
 
+#define ARQUIVO_CLIENTES = "clientes.dat";
+#define ARQUIVO_SERVICOS = "servicos.dat";
+#define ARQUIVO_CLIENTE_SERVICO = "cliente_servico.dat";
+
 // =============== Estruturas de dados =======================
 typedef struct {
     int dia, mes, ano;
@@ -43,15 +47,56 @@ void submenu_servicos();
 void submenu_cliente_servico();
 void submenu_relatorios();
 
-// arquivos
+
+// Funções de arquivo para clientes
 void carregar_clientes();
 void salvar_clientes();
 
-//clientes
+// Funções de arquivo para serviços
+void carregar_servicos();
+void salvar_servicos();
+
+// Funções de arquivo para cliente_servico
+void carregar_cliente_servicos();
+void salvar_cliente_servicos();
+
+
+//Funções para clientes
 void listar_todos_clientes();
 void listar_cliente_especifico();
-void incluir_cliente();
-void alterar_cliente();
-void excluir_cliente();
+int incluir_cliente();
+int alterar_cliente();
+int excluir_cliente();
 int buscar_cliente_por_cpf(char cpf[]);
 int cliente_existe(char cpf[]);
+
+// Funções para serviços
+void listar_todos_servicos();
+void listar_servico_especifico();
+int incluir_servico();
+int alterar_servico();
+int excluir_servico();
+int buscar_servico_por_codigo(int codigo);
+int servico_existe(int codigo);
+
+// Funções para cliente_servico
+void listar_todos_cliente_servico();
+void listar_cliente_servico_especifico();
+int incluir_cliente_servico();
+int alterar_cliente_servico();
+int excluir_cliente_servico();
+int buscar_cliente_servico(char cpf[], int codigo, Data data);
+int cliente_servico_existe(char cpf[], int codigo, Data data);
+
+// Funções de relatórios
+void relatorio_clientes_servico_ultimo_mes();
+void relatorio_servicos_data_especifica();
+void relatorio_servicos_periodo();
+
+
+// Funções auxiliares
+// não sei se vou precisar
+Data ler_data();
+void imprimir_data(Data data);
+int comparar_datas(Data d1, Data d2);
+int data_no_ultimo_mes(Data data);
